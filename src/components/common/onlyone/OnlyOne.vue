ae198dddcac3842cbd14af0a06f9d64f6630d22f
Dagenjun-kami <58912363+Dagenjun-kami@users.noreply.github.com>
2020-10-26 18:32:15 +0800

    修复播放按钮大小位置

--- src/components/common/onlyone/OnlyOne.vue
+++ src/components/common/onlyone/OnlyOne.vue
@@ -2,6 +2,11 @@
   <div class="onlyone">
     <TitleHeader name="独家放送"></TitleHeader>
     <MusicListItem v-for="(item,index) in onlylist" :key="index" :item="item" :wth="28">
+      <div slot="music-btm">
+        <div class="btm">
+            <img src="~assets/img/playMusic/cover_play.png" />
+      </div>
+      </div>
       <div slot="music-t"></div>
     </MusicListItem>
   </div>
@@ -41,5 +46,23 @@ export default {
   display: flex;
   justify-content: space-around;
   flex-wrap: wrap;
+}
+.btm {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 22%;
+  /* 相对自身偏移 */
+  transform: translate(-50%, -50%);
+}
+.btm img {
+  width: 100%;
+  opacity: 0;
+  transform: scale(0.7);
+  transition: all 0.5s;
+}
+.itemImg:hover .btm img {
+  opacity: 1;
+  transform: scale(1);
 }
 </style>
